92ef9cb9cce1c7360093ea7f082a0c662605439a
Douglas <drpowell23@gmail.com>
2025-12-11 16:16:20 -0500

    Add s3_bucket module from private registry

--- main.tf
+++ main.tf
@@ -127,3 +127,10 @@ module "ec2_instances" {
     environment = "devdevelopment"
   }
 }
+
+module "s3_bucket" {
+  source  = "app.terraform.io/policy-as-code-training/terraform-aws-s3-bucket-drp/aws"
+  version = "1.0.0"
+  
+  bucket_name = "terraform-aws-s3-bucket-drp"
+}
