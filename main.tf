71389d33e515bc931fa360b3e00da4fa7218b1e0
Douglas <drpowell23@gmail.com>
2025-12-15 10:23:15 -0500

    Edited repository

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
 terraform {
+  cloud {
+    organization = "policy-as-code-training"
+    workspaces {
+      name = "tf-vault-qa-drp"
+    }
+  }
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-west-1"
+  region  = "us-east-1"
 }
 
 data "aws_availability_zones" "available" {
@@ -109,8 +115,8 @@ module "elb_http" {
 module "ec2_instances" {
   source = "./modules/aws-instance"
 
-  instance_count     = 2
-  instance_type      = "t2.micro"
+  instance_count     = var.instance_count
+  instance_type      = var.instance_type
   subnet_ids         = module.vpc.private_subnets[*]
   security_group_ids = [module.app_security_group.this_security_group_id]
 
